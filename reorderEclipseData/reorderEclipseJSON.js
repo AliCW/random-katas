a2feee4600d58a687c8d1ed180c3d107d10a778e
Ali <aliwhitrow1@gmail.com>
2023-11-25 19:29:01 +0000

    function updated to write new JSON files from input

--- reorderEclipseData/reorderEclipseJSON.js
+++ reorderEclipseData/reorderEclipseJSON.js
@@ -1,10 +1,22 @@
-const reorderEclipseJSON = (JSON) => {
-    let output = [...JSON]
-    output.sort((a, b) => {
-        return new Date(a.date.slice(0, 7)) - new Date(b.date.slice(0, 7));
-    })
-    return output
-}
+//const fs = require('fs');
+
+    const reorderEclipseJSON = (json, fileName) => {
+    
+        let output = [...json];
+    
+        output.sort((a, b) => {
+            return new Date(a.date.slice(0, 7)) - new Date(b.date.slice(0, 7));
+        });
+    
+        // output = JSON.stringify(output);
+    
+        // fs.writeFile(fileName, output, (error) => {
+        //     if (error)
+        //     console.error(error)
+        // });
+    
+        return output;
+    };
 
 module.exports = { reorderEclipseJSON }
 
