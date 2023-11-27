43cd2cd67e8b18443ca1df59982481d4ef77c4d1
Ali <aliwhitrow1@gmail.com>
2023-11-27 23:12:20 +0000

    eclipse function updated

--- reorderEclipseData/reorderEclipseJSON.js
+++ reorderEclipseData/reorderEclipseJSON.js
@@ -1,4 +1,7 @@
-//const fs = require('fs');
+const hybrid = require('./data/all-hybrid-eclipses.json');
+const annular = require('./data/annular-eclipses.json');
+const total = require('./data/total-eclipses.json');
+const fs = require('fs');
 
     const reorderEclipseJSON = (json, fileName) => {
     
@@ -8,15 +11,21 @@
             return new Date(a.date.slice(0, 7)) - new Date(b.date.slice(0, 7));
         });
     
-        // output = JSON.stringify(output);
+        output = JSON.stringify(output);
     
-        // fs.writeFile(fileName, output, (error) => {
-        //     if (error)
-        //     console.error(error)
-        // });
+        fs.writeFile(fileName, output, (error) => {
+            if (error)
+            console.error(error)
+        });
     
         return output;
     };
+
+const hybridFileName = 'all-hybrid-eclipses.json';
+const annularFileName = 'all-annular-eclipses.json';
+const totalFileName = 'all-total-eclipses.json';
+
+recorderEclipseJSON()
 
 module.exports = { reorderEclipseJSON }
 
