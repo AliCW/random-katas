09497ab38cade0d10614b04b34cffb4355ac46fd
AliCW <aliwhitrow1@gmail.com>
2024-03-14 00:27:48 +0000

    simple-string-expansion

---
+++ simple-string-expansion/__tests__/simpleStringExpansion.test.js
@@ -0,0 +1,52 @@
+const { stringExpansion } = require('../simpleStringExpansion');
+
+describe('Expand the alphanumeric string as per the digits given, the return argument should have no digits', () => {
+    test('Given "3abc", return "aaabbbccc"', () => {
+        const s = '3abc';
+        expect(stringExpansion(s)).toBe('aaabbbccc');
+    });
+    test('Given "", return ""', () => {
+        const s = '';
+        expect(stringExpansion(s)).toBe('');
+    });
+    test('Given "3D2a5d2f", return DDDaadddddff', () => {
+        const s = '3D2a5d2f';
+        expect(stringExpansion(s)).toBe('DDDaadddddff');
+    });
+    test('Given "0d0a0v0t0y", return ""', () => {
+        const s = '0d0a0v0t0y';
+        expect(stringExpansion(s)).toBe('');
+    });
+    test('Given "3d332f2a", return dddffaa', () => {
+        const s = '3d332f2a';
+        expect(stringExpansion(s)).toBe('dddffaa');
+    });
+    test('Given "abcde", return "abcde"', () => {
+        const s = 'abcde';
+        expect(stringExpansion(s)).toBe('abcde');
+    });
+    test('Given "a2bcde", return "abbccddee"', () => {
+        const s = 'a2bcde';
+        expect(stringExpansion(s)).toBe('abbccddee');
+    });
+    test('Given "5YQ7A28r8Cr6m5Kq", return YYYYYQQQQQAAAAAAArrrrrrrrCCCCCCCCrrrrrrrrmmmmmmKKKKKqqqqq', () => {
+        const s = '5YQ7A28r8Cr6m5Kq';
+        expect(stringExpansion(s)).toBe('YYYYYQQQQQAAAAAAArrrrrrrrCCCCCCCCrrrrrrrrmmmmmmKKKKKqqqqq');
+    });
+    test('Given "0Qj3ok34vK8DYWDs", return oookkkvvvvKKKKDDDDDDDDYYYYYYYYWWWWWWWWDDDDDDDDssssssss', () => {
+        const s = '0Qj3ok34vK8DYWDs';
+        expect(stringExpansion(s)).toBe('oookkkvvvvKKKKDDDDDDDDYYYYYYYYWWWWWWWWDDDDDDDDssssssss');
+    });
+    test('Given "S1OnbiNh47qn1Q56", return SOnbiNhqqqqqqqnnnnnnnQ', () => {
+        const s = 'S1OnbiNh47qn1Q56';
+        expect(stringExpansion(s)).toBe('SOnbiNhqqqqqqqnnnnnnnQ');
+    });
+    test('Given "A3IkANV0U0bR51nW", return AIIIkkkAAANNNVVVnW', () => {
+        const s = 'A3IkANV0U0bR51nW';
+        expect(stringExpansion(s)).toBe('AIIIkkkAAANNNVVVnW');
+    });
+    test('Given "OlbG97skvn04hM81", return OlbGssssssskkkkkkkvvvvvvvnnnnnnnhhhhMMMM', () => {
+        const s = 'OlbG97skvn04hM81';
+        expect(stringExpansion(s)).toBe('OlbGssssssskkkkkkkvvvvvvvnnnnnnnhhhhMMMM');
+    });
+});
---
+++ simple-string-expansion/simpleStringExpansion.js
@@ -0,0 +1,30 @@
+const stringExpansion = (s) => {
+
+    let output = '';
+
+    if(s.search(/[0-9]/g) === -1) return s;
+
+    for(let k = 0; k < s.length; k++) {
+        if(/[0-9]/g.test(Number(s[k])) === false) {
+            output += s[k];
+        } else {
+            break;
+        };
+    };
+
+    for(let i = 0; i < s.length; i++) {
+        if(/[0-9]/g.test(Number(s[i])) === true) {
+            for(let j = i + 1; j < s.length; j++) {
+                if(s[j] === undefined) break;
+                if(/[0-9]/g.test(Number(s[j])) === true) break;
+                let append = s[j].repeat(s[i]);
+                output += append;
+            };
+        };
+    };
+    
+    return output;
+
+};
+
+module.exports = { stringExpansion };
