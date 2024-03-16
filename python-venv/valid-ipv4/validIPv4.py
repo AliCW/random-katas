5a78ecacd474e7e766548f2843622e1601beab5c
AliCW <aliwhitrow1@gmail.com>
2024-03-16 01:29:27 +0000

    valid-IPv4 - python-regex-solution

---
+++ python-venv/valid-ipv4/validIPv4.py
@@ -0,0 +1,26 @@
+import re
+
+def validIPv4(str):
+
+    array = str.split(".")
+
+    if(len(array)!= 4):
+        return False
+    
+    for i in range(len(array)):
+        print(re.search("[!0-9]", array[i]))
+        if re.search("[0-9]", array[i]) == None:
+            return False
+        if bool(re.search("[^0-9]", array[i])) == True:
+            return False
+
+        if len(array[i]) > 1:
+            if re.search("[1-9]", array[i][0]) == None:
+                return False 
+            if re.search(" +", array[i]) != None:
+                return False
+            
+        if(int(array[i]) < 0 or int(array[i]) > 255):
+            return False
+
+    return True
