c916ef3e9c65ccf27ae4720e55903ae225ea7418
AliCW <aliwhitrow1@gmail.com>
2024-05-01 16:10:41 +0100

    stone_pickaxes - python (refactor)

--- python-venv/stone_pickaxes/stone_pickaxes.py
+++ python-venv/stone_pickaxes/stone_pickaxes.py
@@ -3,26 +3,15 @@ def stone_pick(array):
     if len(array) <= 3:
         return 0
     
-    wood = 0
-    sticks = 0
-    cobble = 0
-
-    for i in range(len(array)):
-        if array[i] == 'Wood':
-            wood += 1
-        elif array[i] == 'Sticks':
-            sticks += 1
-        elif array[i] == 'Cobblestone':
-            cobble += 1
+    wood = array.count('Wood')
+    sticks = array.count('Sticks')
+    cobble = array.count('Cobblestone')
 
     if wood > 1:
         sticks += wood * 4
 
-    if sticks <= 1 or cobble <= 2:
-        return 0
-
     output = 0
-    for j in range(len(array)):
+    for i in range(len(array)):
         if sticks <= 1 or cobble <= 2:
             return output
         output += 1
