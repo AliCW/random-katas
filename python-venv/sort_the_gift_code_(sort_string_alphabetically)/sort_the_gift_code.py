587f8ded0742ac6a4978c188a8bcca9c291409c5
AliCW <aliwhitrow1@gmail.com>
2024-05-21 15:37:20 +0100

    sort_gift_code - python

---
+++ python-venv/sort_the_gift_code_(sort_string_alphabetically)/sort_the_gift_code.py
@@ -0,0 +1,4 @@
+def sort_gift_code(string):
+    return ''.join(sorted(string))
+
+#https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/train/python
---
+++ python-venv/sort_the_gift_code_(sort_string_alphabetically)/sort_the_gift_code_test.py
@@ -0,0 +1,27 @@
+from sort_the_gift_code import sort_gift_code
+
+def test_1():
+    string = "abcdef"
+    assert sort_gift_code(string) == "abcdef"
+
+def test_2():
+    string = "pqksuvy"
+    assert sort_gift_code(string) == "kpqsuvy"
+
+def test_3():
+    string = "zyxwvutsrqponmlkjihgfedcba"
+    assert sort_gift_code(string) == "abcdefghijklmnopqrstuvwxyz"
+
+def test_4():
+    string = "qgcahkxfljwuv"
+    assert sort_gift_code(string) == "acfghjklquvwx"
+
+def test_5():
+    string = "avbncrftxpkdisl"
+    assert sort_gift_code(string) == "abcdfiklnprstvx"
+
+def test_6():
+    string = "qgdcezofxprtkvbhmnjyilau"
+    assert sort_gift_code(string) == "abcdefghijklmnopqrtuvxyz"
+
+#https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/train/python
