aa7d967973cfe4d8962158a3f3e4b929422a715f
Ali <53982566+AliCW@users.noreply.github.com>
2024-06-28 14:20:50 +0100

    Delete twoOldestAges directory for refactor

--- twoOldestAges/twoOldestAges.js
+++
@@ -1,9 +0,0 @@
-function twoOldestAges(ages){
-    let sortArray = [...ages]
-    sortArray = sortArray.sort((a, b) => a - b).reverse()
-    return sortArray.slice(0, 2).reverse()
-  }
-  
-module.exports = { twoOldestAges }
-
-//https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
