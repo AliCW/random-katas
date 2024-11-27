f60a69927f8491da24becf5634ee57fff7d9e280
Ali <aliwhitrow1@gmail.com>
2024-11-27 15:35:45 +0000

    simple-encryption-qwerty - decrypt function working

--- simple-encryption-qwerty/__tests__/simpleEncryptionQwerty.test.js
+++ simple-encryption-qwerty/__tests__/simpleEncryptionQwerty.test.js
@@ -1,55 +1,99 @@
 const { encrypt, decrypt } = require('../simpleEncryptionQwerty');
 
-describe('Given a string & number key, encrypt the given string (attach rules )', () => {
-    test('Given "A" & 111, return "S"', () => {
+describe(`Given a string & number key, encrypt the given string (attach rules )
+    The crypting-regions are these 3 lines from your keyboard:
+    1. "qwertyuiop"
+    2. "asdfghjkl"
+    3. "zxcvbnm,."
+
+    If a char of the string is not in any of these regions, take the char direct in the output.
+    If a char of the string is in one of these regions: Move it by the part of the key in the 
+    region and take this char at the position from the region. 
+    If the movement is over the length of the region, continue at the beginning.
+    The encrypted char must have the same case like the decrypted char! 
+    So for upperCase-chars the regions are the same, but with pressed "SHIFT"!
+
+    The Encryption-Key is an integer number from 000 to 999. E.g.: 127
+
+    The first digit of the key (e.g. 1) is the movement for the first line.
+    The second digit of the key (e.g. 2) is the movement for the second line.
+    The third digit of the key (e.g. 7) is the movement for the third line.`, () => {
+    test('Encrypt test - Given "A" & 111, return "S"', () => {
         const string = 'A';
         const key = 111;
         expect(encrypt(string, key)).toBe('S');
     });
-    test('Given "Abc" & 212, return "Smb"', () => {
+    test('Encrypt test - Given "Abc" & 212, return "Smb"', () => {
         const string = 'Abc';
-        const key = 212
+        const key = 212;
         expect(encrypt(string, key)).toBe('Smb');
     });
-    test('Given "Wave" & 0, return "Wave"', () => {
+    test('Encrypt test - Given "Wave" & 0, return "Wave"', () => {
         const string = 'Wave';
-        const key = 0
+        const key = 0;
         expect(encrypt(string, key)).toBe('Wave');
     });
-    test('Given "Wave" & 345, return "Tg.y"', () => {
+    test('Encrypt test - Given "Wave" & 345, return "Tg.y"', () => {
         const string = 'Wave';
-        const key = 345
+        const key = 345;
         expect(encrypt(string, key)).toBe('Tg.y');
     });
-    test('Given "Ball" & 134, return ">fdd"', () => {
-        const string = "Ball";
-        const key = 134
+    test('Encrypt test - Given "Ball" & 134, return ">fdd"', () => {
+        const string = 'Ball';
+        const key = 134;
         expect(encrypt(string, key)).toBe('>fdd');
     });
-    test('Given "Ball" & 444, return ">gff"', () => {
-        const string = "Ball";
+    test('Encrypt test - Given "Ball" & 444, return ">gff"', () => {
+        const string = 'Ball';
         const key = 444;
         expect(encrypt(string, key)).toBe('>gff');
     });
-    test('Given "This is a test." & 348, return "Iaqh qh g iyhi,"', () => {
-        const string = "This is a test.";
-        const key = 348
+    test('Encrypt test - Given "This is a test." & 348, return "Iaqh qh g iyhi,"', () => {
+        const string = 'This is a test.';
+        const key = 348;
         expect(encrypt(string, key)).toBe('Iaqh qh g iyhi,');
     });
-    test(`Given "Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution." & 583
+    test(`Encrypt test - Given "Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution." & 583
             return "Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c"`, () => {
-        const string = "Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.";
+        const string = 'Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.';
         const key = 583;
-        expect(encrypt(string, key)).toBe('Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c')
-    })
+        expect(encrypt(string, key)).toBe('Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c');
+    });
+    test('Decrypt test - Given "S" & 111, return "A"', () => {
+        const string = 'S';
+        const key = 111;
+        expect(decrypt(string, key)).toBe('A');
+    });
+    test('Decrypt test - Given "Smb" & 212, return "Abc"', () => {
+        const string = 'Smb';
+        const key = 212;
+        expect(decrypt(string, key)).toBe('Abc');
+    });
+    test('Decrypt test - Given "Wave" & 000, return "Wave"', () => {
+        const string = 'Wave';
+        const key = 0;
+        expect(decrypt(string, key)).toBe('Wave');
+    });
+    test('Decrypt test - Given "Tg.y" & 345, return "Wave"', () => {
+        const string = 'Tg.y';
+        const key = 345;
+        expect(decrypt(string, key)).toBe('Wave');
+    });
+    test('Decrypt test - Given ">fdd" & 134, return "Ball"', () => {
+        const string = ">fdd";
+        const key = 134;
+        expect(decrypt(string, key)).toBe('Ball');
+    });
+    test('Given "vM{Gwj bmec" & 958, return "b<{Les n,rv"', () => {
+        const string = 'vM{Gwj bmec';
+        const key = 958;
+        expect(decrypt(string, key)).toBe('b<{Les n,rv');
+    });
+    test('Given "Iaqh qh g iyhi," & 348, return "This is a test."', () => {
+        const string = "Iaqh qh g iyhi,";
+        const key = 348;
+        expect(decrypt(string, key)).toBe('This is a test.');
+    });
 });
 
 //https://www.codewars.com/kata/57f14afa5f2f226d7d0000f4/train/javascript
-
-
-// 1. "qwertyuiop"
-// 2. "asdfghjkl"
-// 3. "zxcvbnm,."
-
-
-//assert.strictEqual(encrypt(", 583), "Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c")
--- simple-encryption-qwerty/simpleEncryptionQwerty.js
+++ simple-encryption-qwerty/simpleEncryptionQwerty.js
@@ -1,5 +1,5 @@
 const encrypt = (string,number) => {
-    let num = ""
+    let num = "";
 
     if(number.toString().length === 2){
         num += "0" + number.toString();
@@ -13,75 +13,120 @@ const encrypt = (string,number) => {
     const cypher3 = "zxcvbnm,.";
 
      return string.split('').map((element) => {
+
         if(cypher1.includes(element.toLowerCase())){
             encryptIndex = cypher1.indexOf(element.toLowerCase()) + Number(num[0]);
             if(encryptIndex > cypher1.length - 1){
-                encryptIndex = encryptIndex - cypher1.length
+                encryptIndex = encryptIndex - cypher1.length;
             }
-
             if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
-                return cypher1[encryptIndex].toUpperCase()
+                return cypher1[encryptIndex].toUpperCase();
             } else {
-                return cypher1[encryptIndex]
+                return cypher1[encryptIndex];
             }
         }
+
         else if(cypher2.includes(element.toLowerCase())){
             encryptIndex = cypher2.indexOf(element.toLowerCase()) + Number(num[1]);
             console.log(element, encryptIndex);
             if(encryptIndex > cypher2.length - 1){
-                encryptIndex = encryptIndex - cypher2.length
+                encryptIndex = encryptIndex - cypher2.length;
             }
-
-
             if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
-                return cypher2[encryptIndex].toUpperCase()
+                return cypher2[encryptIndex].toUpperCase();
             } else {
-                return cypher2[encryptIndex]
+                return cypher2[encryptIndex];
             }
-
-
         }
+
         else if(cypher3.includes(element.toLowerCase())){
             encryptIndex = cypher3.indexOf(element.toLowerCase()) + Number(num[2]);
             if(encryptIndex > cypher3.length - 1){
-                encryptIndex = encryptIndex - cypher3.length
+                encryptIndex = encryptIndex - cypher3.length;
             }
-
             if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
-                if(cypher3[encryptIndex] === ",") return "<"
-                if(cypher3[encryptIndex] === ".") return ">"
-                return cypher3[encryptIndex].toUpperCase()
+                if(cypher3[encryptIndex] === ",") return "<";
+                if(cypher3[encryptIndex] === ".") return ">";
+                return cypher3[encryptIndex].toUpperCase();
             } else {
-                return cypher3[encryptIndex]
+                return cypher3[encryptIndex];
             }
         }
-        return element
-    }).join('')
+
+        return element;
+    }).join('');
 
 };
 
 const decrypt = (string,number) => {
-    
+    let num = "";
+
+    if(number.toString().length === 2){
+        num += "0" + number.toString();
+    } else if (number.toString().length === 1){
+        num += "00" + number.toString();
+    } else {
+        num = number.toString();
+    }
+    const cypher1 = "poiuytrewq";
+    const cypher2 = "lkjhgfdsa";
+    const cypher3 = ".,mnbvcxz";
+
+     return string.split('').map((element) => {
+
+        if(cypher1.includes(element.toLowerCase())){
+            encryptIndex = cypher1.indexOf(element.toLowerCase()) + Number(num[0]);
+            if(encryptIndex > cypher1.length - 1){
+                encryptIndex = encryptIndex - cypher1.length;
+            }
+            if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
+                return cypher1[encryptIndex].toUpperCase();
+            } else {
+                return cypher1[encryptIndex];
+            };
+        }
+
+        else if(cypher2.includes(element.toLowerCase())){
+            encryptIndex = cypher2.indexOf(element.toLowerCase()) + Number(num[1]);
+            console.log(element, encryptIndex);
+            if(encryptIndex > cypher2.length - 1){
+                encryptIndex = encryptIndex - cypher2.length;
+            }
+            if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
+                return cypher2[encryptIndex].toUpperCase();
+            } else {
+                return cypher2[encryptIndex];
+            };
+        }
+
+        else if(cypher3.includes(element.toLowerCase())){
+            encryptIndex = cypher3.indexOf(element.toLowerCase()) + Number(num[2]);
+            if(encryptIndex > cypher3.length - 1){
+                encryptIndex = encryptIndex - cypher3.length;
+            };
+            if(element.charCodeAt() >= 65 && element.charCodeAt() <= 90){
+                if(cypher3[encryptIndex] === ",") return "<";
+                if(cypher3[encryptIndex] === ".") return ">";
+                return cypher3[encryptIndex].toUpperCase();
+            } else {
+                return cypher3[encryptIndex];
+            };
+        }
+
+        else if(element === ">"){
+            encryptIndex = Number(num[2]);
+            return cypher3[encryptIndex].toUpperCase();
+        }
+        else if(element === "<"){
+            encryptIndex = Number(num[2]);
+            return cypher3[encryptIndex].toUpperCase();
+        };
+
+        return element;
+    }).join('');
 
 };
 
 module.exports = { encrypt, decrypt };
 
 //https://www.codewars.com/kata/57f14afa5f2f226d7d0000f4/train/javascript
-
-
-
-
-//check length of number & add zeroes if one or two digits long
-
-
-//declare the cyphers as individual lines
-// assign cyphers to number - 123, 1=cypher1, 2=cypher2 etc
-
-// declare blank output
-//split input string & loop / iterate
-
-//check which cypher current element is in
-//find index of current element 
-//add assigned number to the index & add that element to output
-//if adding the assigned number exceeds the index - do something maths - current number - cypher length = return index
